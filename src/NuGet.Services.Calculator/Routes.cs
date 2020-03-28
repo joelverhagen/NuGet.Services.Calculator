4e80a809dfc91a3c7ce6d97dbf2a3ec33674ad9d
Joel Verhagen <joel.verhagen@gmail.com>
2020-03-28 14:47:56 -0700

    Add an edit versions button

---
+++ src/NuGet.Services.Calculator/Routes.cs
@@ -0,0 +1,9 @@
+﻿namespace NuGet.Services.Calculator
+{
+    public static class Routes
+    {
+        public const string Index = "/";
+        public const string FindBestVersionMatch = "/find-best-version-match";
+        public const string FindBestPackageVersionMatch = "/package-versions";
+    }
+}
